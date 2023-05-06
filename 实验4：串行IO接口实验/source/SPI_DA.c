e1a86c7ef1602953f418be632b4cbab6d0e11a9b
林子垚 <62921296+Sen-Yao@users.noreply.github.com>
2023-05-06 20:27:52 +0800

    Update SPI_DA.c

--- 实验4：串行IO接口实验/source/SPI_DA.c
+++ 实验4：串行IO接口实验/source/SPI_DA.c
@@ -2,7 +2,7 @@
 // SPI 用于连接 DA 模块，在板子上，将 DA 接 Pmod JA
 
 // 在硬件平台搭建中，将 timer 接中断 0，SPI 接中断 1，switch 接中断 2；sw 为 GPIO_0 的 0
-// 
+//
 // DA 模块的三根线分别为 SS0,SCLK0,MOSI
 
 
@@ -29,29 +29,32 @@ int to_set_value; // 在本次试验中，采用减计数。由于本次实验�
 
 int main()
 {
-	xil_printf("\r\nRunning!\r\n");
+	// xil_printf("\r\nRunning!\r\n");
 	Initialize();
     while(1)
     {
     	if(flag_Sw)  // 若拨动 switch 开关，则打印相关信息
         {
-            xil_printf("flag_Sw = %X, Switch Interrupt Triggered! The result is 0x%X\n\r", flag_Sw, sw);
+            xil_printf("flag_Sw = %X, Switch Interrupt Triggered! The result is 0x%X\n", flag_Sw, sw);
             // 开关拨动，说明此时应写入的数据 to_set_value 发生了改变。首先算出需要的对应的 to_set_value。
             // 题目要求周期最长为 1s，最短为 60ms 左右。由于此 DA 芯片输入最大为 4096，如果周期为 1s，而计时周期为 100 MHz，则每过 1s/4096 = 244 约等于 256 个周期。因此我们可以将 to_set_value 设置为：
             to_set_value = 16 * (sw + 1);
-            xil_printf("Now the period is %fs\n\r", to_set_value * 4.096);
+            xil_printf("Now the period is %d\n", to_set_value * 4);
             // 重新设置完周期后，在下一个 Timer 中断时，就会装在新的 to_set_value，从而开始采用新的周期
             flag_Sw = 0;
         }
         if(flag_Timer)
         {
-            xil_printf("time's up!\n\r");
+            //xil_printf("time's up!\n");
             flag_Timer = 0;
+            // xil_printf("Now the period is %d\n", to_set_value * 4);
         }
-    	if(flag_SPI) 
+    	if(flag_SPI)
         {
-            xil_printf("SPI Interrupt Triggered!\n\r");
+            // xil_printf("SPI Interrupt Triggered!\n");
             flag_SPI = 0;
+    	    Xil_Out16(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_DTR_OFFSET,volt);//启动 SPI 使能，产生时钟和片选信号。数字信号对应的有效位为低 12 位，因此在这里将volt 与 0xfff 相与之后再输出
+    	    xil_printf("volt is %d\n", volt);
         }
 
 
@@ -67,7 +70,7 @@ void Initialize()
     Xil_Out8(XPAR_AXI_GPIO_0_BASEADDR + XGPIO_IER_OFFSET, 0x01);  // 端口 0 打开
     Xil_Out32(XPAR_AXI_GPIO_0_BASEADDR + XGPIO_GIE_OFFSET, XGPIO_GIE_GINTR_ENABLE_MASK);  // sw 中断启动
     // switch 初始化完成
-    
+
     // 以下为 SPI 初始化
     Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_CR_OFFSET, XSP_CR_ENABLE_MASK | XSP_CR_MASTER_MODE_MASK|XSP_CR_CLK_POLARITY_MASK);  // 首先设置 SPI 接口的通信模式。设置 SPI 为主设备，CPOL = 1, CPHA = 0，自动方式，高位优先传送。空闲时始终为高电平，因此在上升沿采样数据
     Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_SSR_OFFSET, 0xfffffffe);  // 设置 SSR 寄存器。由于只连接了一个从设备，因此只需要 SS0 为 0，其余为高电平
@@ -97,8 +100,8 @@ void Initialize()
 
 
 void My_ISR(){
-	xil_printf("Interrupt Triggered!\n");
 	int status;
+	// xil_printf("Now the period is %d\n", to_set_value * 4);
 	status = Xil_In32(XPAR_AXI_INTC_0_BASEADDR + XIN_ISR_OFFSET);  // 读取 ISR
 	if (status & 0x04) // ISR[2] = 1, 说明是 switch 中断
 	{
@@ -109,14 +112,20 @@ void My_ISR(){
 	{
 		xil_printf("SPITriggered!\n");
 		SPIHandler();
+
 	}
     if (status & 0x01) //ISR[0] = 1, 说明是 Timer 中断
 	{
-		xil_printf("TimerTriggered!\n");
-		SPIHandler();
+		//xil_printf("TimerTriggered!\n");
+    	// xil_printf("Now the period is %d\n", to_set_value * 4);
+		TimerHandler();
+
 	}
     // 写 INTC 中断响应寄存器
     Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IAR_OFFSET, Xil_In32(XPAR_AXI_INTC_0_BASEADDR + XIN_ISR_OFFSET));
+    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IAR_OFFSET, 0xffffffff);  // 清除所有中断请求
+    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IER_OFFSET, 0x07); // intc[2:0] 启动
+    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_MER_OFFSET, 0x07);// intc[2:0] 启动
 }
 
 void SwitchHandler()
@@ -133,9 +142,12 @@ void SPIHandler()
 	flag_SPI = 1;
 	// 清除 SPI 中断寄存器
     Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_IISR_OFFSET, Xil_In32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_IISR_OFFSET));
-    Xil_Out16(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_DTR_OFFSET, volt & 0xfff);//启动 SPI 使能，产生时钟和片选信号。数字信号对应的有效位为低 12 位，因此在这里将volt 与 0xfff 相与之后再输出
-    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_ISR_OFFSET, 0x00);
-    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IER_OFFSET, 0x07); // 写 1 清除中断标志位
+    Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IAR_OFFSET, Xil_In32(XPAR_AXI_INTC_0_BASEADDR + XIN_ISR_OFFSET));
+    Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_CR_OFFSET, XSP_CR_ENABLE_MASK | XSP_CR_MASTER_MODE_MASK|XSP_CR_CLK_POLARITY_MASK);  // 首先设置 SPI 接口的通信模式。设置 SPI 为主设备，CPOL = 1, CPHA = 0，自动方式，高位优先传送。空闲时始终为高电平，因此在上升沿采样数据
+    Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_SSR_OFFSET, 0xfffffffe);  // 设置 SSR 寄存器。由于只连接了一个从设备，因此只需要 SS0 为 0，其余为高电平
+    Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_IIER_OFFSET, XSP_INTR_TX_EMPTY_MASK);  // 开放 SPI 发送寄存器中断，若发送完数字信号，则 SPI 产生中断
+    Xil_Out32(XPAR_AXI_QUAD_SPI_0_BASEADDR + XSP_DGIER_OFFSET, XSP_GINTR_ENABLE_MASK);  // 开启 SPI 接口的中断输出。
+
 
 }
 
@@ -144,16 +156,21 @@ void TimerHandler()
     flag_Timer = 1;
 	// 清除 SPI 中断寄存器
     volt++; // 由于要求输出锯齿波，因此每输出一次，电压++，
+    if (volt == 4095)
+    {
+    	volt = 0;
+    }
 
+    to_set_value = 160000 * (sw + 1);
+    // xil_printf("Now the period is %d\n", to_set_value * 4);
     // 每次 Timer 中断触发后，理论上需要重新装载计时器
     int tcsr0 = Xil_In32(XPAR_TMRCTR_0_BASEADDR + XTC_TCSR_OFFSET);  // tcsr0 意思是 定时器 0 控制寄存器，有 32 位，但只有 0～10 位有效。这里我们读出 TCSR 寄存器，写入这里的变量 tcsr0
     Xil_Out32(XPAR_TMRCTR_0_BASEADDR + XTC_TLR_OFFSET, to_set_value);  // TLR 为预置数寄存器，这里需要写 LDR 寄存器，将其赋值为 to_set_value 配置计数初始值
-
-    tcsr0 = Xil_In32(XPAR_TMRCTR_0_BASEADDR+XTC_TCSR_OFFSET);  // 更新 tcsr0 中的数据
+    tcsr0 = Xil_In32(XPAR_TMRCTR_0_BASEADDR+XTC_TLR_OFFSET);  // 更新 tcsr0 中的数据
     Xil_Out32(XPAR_TMRCTR_0_BASEADDR + XTC_TCSR_OFFSET, tcsr0 | XTC_CSR_LOAD_MASK);  // LOAD = 1，将 TLR 装载到 TCR，准备开始计数
     Xil_Out32(XPAR_TMRCTR_0_BASEADDR + XTC_TCSR_OFFSET, tcsr0 | ((XTC_CSR_ENABLE_TMR_MASK | XTC_CSR_DOWN_COUNT_MASK | XTC_CSR_AUTO_RELOAD_MASK) & (~XTC_CSR_LOAD_MASK)));  // ENT = 1，运行定时器，UDT = 1, 减计数 ，自动装载:ARHT=1,LOAD =0;
     Xil_Out32(XPAR_TMRCTR_0_BASEADDR + XTC_TCSR_OFFSET, Xil_In32(XPAR_TMRCTR_0_BASEADDR + XTC_TCSR_OFFSET) | XTC_CSR_INT_OCCURED_MASK|XTC_CSR_ENABLE_INT_MASK); // 清除中断标志：写 TINT=1（这是 Timer 的特定寄存器功能）;
     Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_ISR_OFFSET, 0x00);
     Xil_Out32(XPAR_AXI_INTC_0_BASEADDR + XIN_IER_OFFSET, 0x07); // 写 1 清除中断标志位
-    
+
 }
